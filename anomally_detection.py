5071311aa304b2143a36a243c3c9b13452de94a2
Martin Benes <benesm44@fit.cvut.cz>
2023-04-01 22:42:19 +0200

    implement detect_anomallies

--- anomally_detection.py
+++ anomally_detection.py
@@ -1,13 +1,8 @@
-import pandas as pd
-from sklearn.cluster import dbscan
-from preprocessing import preprocess
-from chunk_anomally_detection import *
+from chunk_noise_detection import chunk_noise
 
-def dbscan_chunkus(chunks):
+def detect_anomallies(chunks):
     noise = set()
     for chunk in chunks:
-        # db = DBSCAN(eps=eps, min_samples=min_samples).fit(chunk)
-        # noise_labels = chunk[db.labels_ == -1].index
-        noise_labels = dbscan_chunk(chunk)
+        noise_labels = chunk_noise(chunk)
         noise = noise.union(noise_labels)
     return noise
