09add8d562a224a1e51ae157c14d168603186c3b
Martin Benes <benesm44@fit.cvut.cz>
2023-04-01 22:50:18 +0200

    add docstring for chunk_noise

--- chunk_noise_detection.py
+++ chunk_noise_detection.py
@@ -1,8 +1,20 @@
 from sklearn.cluster import dbscan
 from preprocessing import preprocess
+import numpy as np
 
-def chunk_noise(chunk, eps, min_samples):
+def chunk_noise(chunk, eps=0.5, min_samples=5):
+    """return noise using DBSCAN
+
+    Args:
+        chunk (pd.DataFrame): _description_
+        eps (float, optional): eps parameter of the DBSCAN algorithm. Defaults to 0.5 (DBSCAN default).
+        min_samples (int, optional): min_samples parameter of the DBSCAN algorithm. Defaults to 5.
+
+    Returns:
+        np.array: array of indices corresponding to noise
+    """
+
     chunk = preprocess(chunk)
     labels = dbscan(chunk, eps=eps, min_samples=min_samples)[1]
-    noise_label_indices = chunk[labels == -1].index
+    noise_label_indices = np.array(chunk[labels == -1].index)
     return noise_label_indices
