865942beb5392a49e83f268787f08708e2d22496
Martin Benes <benesm44@fit.cvut.cz>
2023-04-01 22:56:31 +0200

    add docstring for anomally detection

--- anomally_detection.py
+++ anomally_detection.py
@@ -1,6 +1,15 @@
 from chunk_noise_detection import chunk_noise
 
 def detect_anomallies(chunks):
+    """detects anomallies in a list of chunks
+
+    Args:
+        chunks (list): chunks to detect anomallies in
+
+    Returns:
+        set{index}: indeces from the original dataset that could be anomallies
+    """
+
     noise = set()
     for chunk in chunks:
         noise_labels = chunk_noise(chunk)
