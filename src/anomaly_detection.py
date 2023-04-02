c85db8c58c93dde728b21234253f7f020b2b985e
Martin Benes <benesm44@fit.cvut.cz>
2023-04-02 10:42:39 +0200

    correct "anomally" to "anomaly"

--- src/anomaly_detection.py
+++ src/anomaly_detection.py
@@ -7,10 +7,10 @@ def noise_indeces(chunks, mode):
     """returns the indeces of the noise in the original dataset
 
     Args:
-        chunks (list): chunks to detect anomallies in
+        chunks (list): chunks to detect anomalies in
 
     Returns:
-        set{index}: indeces from the original dataset that could be anomallies
+        set{index}: indeces from the original dataset that could be anomalies
     """
 
     if mode not in ['file', 'event']:
@@ -23,8 +23,8 @@ def noise_indeces(chunks, mode):
     return noise
 
 
-def detect_anomallies(data, mode='file', max_chunk_size=10_000, n_runs=5, seed=42):
-    """Detects anomallies in the data
+def detect_anomalies(data, mode='file', max_chunk_size=10_000, n_runs=5, seed=42):
+    """Detects anomalies in the data
 
     Args:
         data (DataFrame): input dataset
@@ -33,21 +33,21 @@ def detect_anomallies(data, mode='file', max_chunk_size=10_000, n_runs=5, seed=4
         seed (int, optional): Random seed. Defaults to 42.
 
     Returns:
-        dict: dictionary of anomally indeces and number their occurences, the higher the number, the more probable the anomally
+        dict: dictionary of anomaly indeces and number their occurences, the higher the number, the more probable the anomaly
     """
 
     if mode not in ['file', 'event']:
         raise ValueError("mode must be 'file' or 'event'")
 
-    anomallies = {}
+    anomalies = {}
     for run in range(n_runs):
         shuffled = data.sample(frac=1, random_state=seed+run)
         chunks = np.array_split(shuffled, math.ceil(shuffled.shape[0] / max_chunk_size))
         
-        anomally_indeces = noise_indeces(chunks, mode)
-        for index in anomally_indeces:
-            anomallies[index] = anomallies.get(index, 0) + 1
+        anomaly_indeces = noise_indeces(chunks, mode)
+        for index in anomaly_indeces:
+            anomalies[index] = anomalies.get(index, 0) + 1
 
-    anomallies = pd.Series(anomallies)
-    anomallies = anomallies[anomallies >= anomallies.max() // 2]
-    return data.iloc[anomallies.index].copy()
+    anomalies = pd.Series(anomalies)
+    anomalies = anomalies[anomalies >= anomalies.max() // 2]
+    return data.iloc[anomalies.index].copy()
